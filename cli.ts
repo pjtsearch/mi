a060c7b15e03ada14e29a4ce50f2bf88a165a266
PJTSearch <pjtsignups@gmail.com>
2020-01-26 16:54:57 -0800

    add basic repl to cli

--- cli.ts
+++ cli.ts
@@ -1,4 +1,10 @@
 import MI from "./src/index.ts"
+import { input } from "https://raw.githubusercontent.com/johnsonjo4531/read_lines/master/input.ts";
+import Scanner from "./src/Scanner.ts"
+import Parser from "./src/Parser.ts"
+import Interpreter from "./src/Interpreter.ts"
+import Stmt from "./src/Stmt.ts"
+import Token from "./src/Token.ts"
 
 if (Deno.args[0] === "ast"){
 	let mi = new MI({dev:false})
@@ -13,4 +19,19 @@ if (Deno.args[0] === "ast"){
 	let mi = new MI({dev:false})
 	//console.log(JSON.stringify(mi.parse(Deno.args[1]),null,1))
 	let answer = mi.interpret((Deno.args[1]))
+}else if (!Deno.args[0]){
+	let mi = new MI({dev:false});
+	(async () => {
+		console.log("-- MI REPL --");
+		const options = {dev:false}
+		const interpreter:Interpreter = new Interpreter([],"",options)
+		while (true){
+			const command:string = await input("> ") as string;
+			const scanner:Scanner = new Scanner(command,options)
+			const tokens:Token[] = scanner.scanTokens()
+			const parser:Parser = new Parser(tokens,command,options)
+			const ast:Stmt[] = parser.parse()
+			console.log(interpreter.interpretOne(ast[0]))
+		}
+	})();
 }
