c6fd7421b2bf4fbf267ac2618140c19e5cb8f38c
PJTSearch <pjtsignups@gmail.com>
2020-01-26 19:06:22 -0800

    add colors for errors in deno

--- src/Error.ts
+++ src/Error.ts
@@ -1,11 +1,29 @@
 import Token from "./Token.ts"
-
+import { bgBlue, red, bold, blue, } from "./utils/color.ts";
 export class BaseError {
 	constructor(type:string,message:string,line?:number,column?:number,source?:string){
-		console.error(`[${type}] ${message} - ${line!==undefined?`${line}`:''}${column!==undefined?`:${column}`:''}
-${source ? source.split("\n")[line-1]:""}
-${source ? new Array(column).join(" ")+"^":""}
-${source ? new Array(column-1).join(" ")+"Here":""}`)
+		//if (Deno){
+			let err = bold(red(`[${type}] `))
+			err+=`${message} `
+			if (line!==undefined) err+=bold(blue(`${line}`))
+			if (column!==undefined) err+=bold(blue(`:${column}`))
+			if (source){
+				err+="\n"+source.split("\n")[line-1]
+				err+=bold(red("\n"+new Array(column).join(" ")+"^"))
+				err+=bold(red("\n"+new Array(column-1).join(" ")+"Here"))
+			}
+			console.error(err)
+		/*}else{
+			let err = `[${type}] `
+			err+=`${message} `
+			if (line!==undefined) err+=`${line}`
+			if (column!==undefined) err+=`:${column}`
+			if (source){
+				err+="\n"+source.split("\n")[line-1]
+				err+="\n"+new Array(column).join(" ")+"^"
+				err+="\n"+new Array(column-1).join(" ")+"Here"
+			}
+		}*/
 	}
 }
 
